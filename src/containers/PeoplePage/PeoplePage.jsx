f252dc10485162997018f21554dee310e966b4af
Denis <chipegin2@gmail.com>
2022-04-12 10:13:27 +0300

    Error handling

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -7,20 +7,26 @@ import styles from './PeoplePage.module.css';
 
 const PeoplePage = () => {
 
-   const [people, setPeople] = useState()
+   const [people, setPeople] = useState(null)
+   const [errorApi, setErrorApi] = useState(false)
 
    const getResource = async (url) => {
       const res = await getApiResource(url)
 
-      const peopleList = res.results.map(({ name, url }) => {
-         const id = getPeopleId(url)
-         const img = getPeopleImage(id)
+      if (res) {
+         const peopleList = res.results.map(({ name, url }) => {
+            const id = getPeopleId(url)
+            const img = getPeopleImage(id)
 
-         return {
-            id, name, img
-         }
-      })
-      setPeople(peopleList);
+            return {
+               id, name, img
+            }
+         })
+         setPeople(peopleList);
+         setErrorApi(false)
+      } else {
+         setErrorApi(true)
+      }
    }
 
    useEffect(() => {
@@ -29,7 +35,15 @@ const PeoplePage = () => {
 
    return (
       <>
-         {people && <PeopleList people={people} />}
+         {errorApi
+            ? <h2>Error</h2>
+            : (
+               <>
+                  <h1>Navigation</h1>
+                  {people && <PeopleList people={people} />}
+               </>
+            )
+         }
       </>
    );
 }
