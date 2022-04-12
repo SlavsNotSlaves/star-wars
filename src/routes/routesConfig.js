7f65b61b17677f9fa1e3082aeb58b499c99770e4
Denis <chipegin2@gmail.com>
2022-04-12 13:02:07 +0300

    error handling 404

--- src/routes/routesConfig.js
+++ src/routes/routesConfig.js
@@ -1,9 +1,11 @@
 import PeoplePage from '@containers/PeoplePage';
 import HomePage from '@containers/HomePage';
+import NotFoundPage from '../containers/NotFoundPage/NotFoundPage';
 
 const routesConfig = [
    { path: '/', element: <HomePage /> },
    { path: '/people', element: <PeoplePage /> },
+   { path: '*', element: <NotFoundPage /> },
 ]
 
 export default routesConfig
