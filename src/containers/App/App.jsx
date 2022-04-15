34383a8611ae7160a92ef49efe91915f7bc45b9f
Denis <chipegin2@gmail.com>
2022-04-15 21:21:13 +0300

    some updates related to deploy

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import routesConfig from '@routes/routesConfig';
 import Header from '@components/Header';
+import { REPO_NAME } from '@constants/repo';
 
-import styles from './App.module.css'
+import styles from './App.module.css';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={`/${REPO_NAME}/`} >
       <div className={styles.wrapper} >
         <Header />
         <Routes>
